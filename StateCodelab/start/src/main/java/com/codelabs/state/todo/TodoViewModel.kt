1d3f123ad53d3f896f5b9579c6f6fda6fe57effd
Minami Munakata <munakata_minami@andfactory.co.jp>
2022-02-12 23:17:07 +0900

    Use State in ViewModel.

--- StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -16,13 +16,20 @@
 
 package com.codelabs.state.todo
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
+    private var currentEditPosition by mutableStateOf(-1)
 
     var todoItems = mutableStateListOf<TodoItem>()
         private set
+
+    val currentEditItem: TodoItem?
+        get() = todoItems.getOrNull(currentEditPosition)
 
     fun addItem(item: TodoItem) {
         todoItems.add(item)
@@ -30,5 +37,23 @@ class TodoViewModel : ViewModel() {
 
     fun removeItem(item: TodoItem) {
         todoItems.remove(item)
+        onEditDone()
+    }
+
+    fun onEditItemSelected(item: TodoItem) {
+        currentEditPosition = todoItems.indexOf(item)
+    }
+
+    fun onEditDone() {
+        currentEditPosition = -1
+    }
+
+    fun onEditItemChange(item: TodoItem) {
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id == item.id) {
+            "You can only change an item with the same id as currentEditItem"
+        }
+
+        todoItems[currentEditPosition] = item
     }
 }
